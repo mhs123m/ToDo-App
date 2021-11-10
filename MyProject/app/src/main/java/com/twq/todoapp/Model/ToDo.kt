36077280d05c3774ead07aa4184bc85374e08a2e
mhs123m <64324346+mhs123m@users.noreply.github.com>
2021-11-10 18:51:29 +0300

    Wednesday... snopshot is working fine, but date and time are crushing. let's ignore them for now and work on the desgin and code for a little bit.

--- MyProject/app/src/main/java/com/twq/todoapp/Model/ToDo.kt
+++ MyProject/app/src/main/java/com/twq/todoapp/Model/ToDo.kt
@@ -1,13 +1,14 @@
 package com.twq.todoapp.Model
 
+import com.google.firebase.Timestamp
 import java.io.Serializable
 
-class ToDo (
-    var id: String,
-    var title:String,
-    var description:String,
-    var dueDate: String,
-    var time:String,
+
+data class ToDo(
+    var id: String?,
+    var title:String?,
+    var description:String?,
+    var dueDate: Timestamp?,
+    var time: Timestamp?,
     var status: Boolean = false
-        ):Serializable {
-}
+        ):Serializable
